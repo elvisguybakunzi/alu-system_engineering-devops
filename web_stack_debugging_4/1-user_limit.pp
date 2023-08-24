3a919cb87274ff38e2adfe5306d17959639a180f
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-08-24 14:04:03 +0200

    adding web stack project 4

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# This program helps in change of the OS configuration
+# Fix open files
 exec { 'Fix hard limit':
   command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
-} 
+}
